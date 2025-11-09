69adc6651d687a29be6b21bd4323ab2cc21d135b
ENOCK YAW ENU <30849211+ekow1@users.noreply.github.com>
2025-11-09 18:03:41 +0000

    added roles & set cookies, fixed cors issue

--- rn-auth-backend/server.js
+++ rn-auth-backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import cors from 'cors';
+import dotenv from 'dotenv';
+
 import authRoutes from './routes/authRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
 import stationRoutes from './routes/stationRoutes.js';
@@ -22,10 +22,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors({
-//     origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3002','http://localhost:3003','http://localhost:3004','http://localhost:3005','http://localhost:3006','http://localhost:3007','http://localhost:3008','http://localhost:3009','http://localhost:3010'],
-//     credentials: true
-// }));
+
 app.use(express.json());
 
 // Swagger UI
