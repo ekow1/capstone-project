9af6ae891d1ab74b4f2bc33f02977d3a5905e7e2
ENOCK YAW ENU <30849211+ekow1@users.noreply.github.com>
2025-11-15 08:53:04 +0000

    dubugging

--- rn-auth-backend/controllers/superAdminController.js
+++ rn-auth-backend/controllers/superAdminController.js
@@ -72,6 +72,10 @@ export const loginSuperAdmin = async (req, res) => {
             });
         }
 
+        console.log('username', username);
+        console.log('password', password);
+        console.log('req.body', req.body);
+
         // Find admin by username
         const admin = await SuperAdmin.findOne({ username: username.toLowerCase() });
         
@@ -81,6 +85,8 @@ export const loginSuperAdmin = async (req, res) => {
                 message: 'Invalid credentials' 
             });
         }
+
+    
 
         // Check if admin is active
         if (!admin.isActive) {
