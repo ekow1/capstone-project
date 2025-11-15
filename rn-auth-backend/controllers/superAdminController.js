d4d21a4c51e25e92f468dcaf9a98488ac33ff3a0
ENOCK YAW ENU <30849211+ekow1@users.noreply.github.com>
2025-11-15 08:49:14 +0000

    fixed the login logic

--- rn-auth-backend/controllers/superAdminController.js
+++ rn-auth-backend/controllers/superAdminController.js
@@ -63,9 +63,9 @@ export const createSuperAdmin = async (req, res) => {
 // Login SuperAdmin
 export const loginSuperAdmin = async (req, res) => {
     try {
-        const { username:serviceNumber, password } = req.body || {};
+        const { serviceNumber:username, password } = req.body || {};
 
-        if (!serviceNumber || !password) {
+        if (!username || !password) {
             return res.status(400).json({ 
                 success: false, 
                 message: 'Username and password are required' 
@@ -73,7 +73,7 @@ export const loginSuperAdmin = async (req, res) => {
         }
 
         // Find admin by username
-        const admin = await SuperAdmin.findOne({ username: serviceNumber.toLowerCase() });
+        const admin = await SuperAdmin.findOne({ username: username.toLowerCase() });
         
         if (!admin) {
             return res.status(404).json({ 
